9bd849716d99155f5564ccd201ba6c58188fe153
lyxHanHan <yixuan.li1@thoughtworks.com>
2020-11-06 23:51:43 +0800

    the third solution of problem3

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,15 +1,17 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Foo {
 
+    @Autowired
     private Bar bar;
 
-    public Foo(Bar bar) {
-        this.bar = bar;
-    }
+//    public Foo(Bar bar) {
+//        this.bar = bar;
+//    }
 
     public void hi() {
         System.out.println("Hi, " + bar.name());
