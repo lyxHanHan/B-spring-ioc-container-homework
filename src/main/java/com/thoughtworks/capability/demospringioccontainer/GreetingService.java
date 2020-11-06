b387cb4ca43bfbd1a4aa13a8b848f637922b2775
lyxHanHan <yixuan.li1@thoughtworks.com>
2020-11-06 23:10:33 +0800

    the first solution of problem2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,10 +1,15 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(value="prototype",proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class GreetingService {
-
+    public GreetingService(){
+        System.out.println("GreetingService");
+    }
     String sayHi() {
         return "hello world";
     }
